fbb6b74347643d5b48ee89047751d54444e3cca7
Randy Rodriguez <Randyr2125@msn.com>
2021-05-03 22:09:42 -0400

    changed the render to show all contacts including new hard coded contact with .create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,6 @@ class ContactsController < ApplicationController
     createContact = Contact.create(first_name: "Cell", last_name: "Saga", phone_number: "999.222.111")
 
     contact = Contact.all
-    render json: contact.as_json
-    # render json: createContact.as_json
+    render json: {contact: contact.as_json}
   end
 end
