203559f03272a087163a4a570778295284cebac2
Randy Rodriguez <Randyr2125@msn.com>
2021-05-03 22:29:29 -0400

    added a POST (.create default) to the seeds file utilizing the db:drop dp:create dp:migrate db:seed cmds

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,7 @@ class ContactsController < ApplicationController
   def get_contacts
     createContact = Contact.create(first_name: "Cell", last_name: "Saga", phone_number: "999.222.111")
 
-    contact = Contact.all
-    render json: {contact: contact.as_json}
+    contacts = Contact.all
+    render json: {contact: contacts.as_json}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create(first_name: "Vageta", last_name: "Saga", phone_number: "999.222.11444")
